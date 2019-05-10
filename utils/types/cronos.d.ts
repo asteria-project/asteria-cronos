3c2ed44a306be5b19247f2e2c0f75ec3ead0635d
Pascal ECHEMANN <pascal@wooz.io>
2019-05-10 15:05:10 +0200

    gitignore update
    Fix util s folder import.

---
+++ utils/types/cronos.d.ts
@@ -0,0 +1,14 @@
+/*!
+ * Asteria Cronos Node Module
+ * Copyright(c) 2019 Pascal ECHEMANN
+ * Apache 2.0 Licensed
+ * This is a part of the Asteria Project: <https://github.com/asteria-project>
+ */
+
+declare module "asteria-cronos" {
+
+import { AbstractAsteriaObject, AsteriaSessionConfig, AsteriaSession, StreamProcessConfig, StreamProcess, AsteriaContext, AsteriaLogger, StreamProcessor, FilterOperator, AsteriaFilter, FilterOperatorNotation, AsteriaStream, AsteriaErrorCode, StreamProcessBuilder, AsteriaError, AsteriaLogLevel, FilterCondition, FilterDefinition, StreamProcessType } from "asteria-gaia";
+import { TransformOptions, Transform, TransformCallback } from "stream";
+import { ReadStream, WriteStream } from "fs";
+    
+/*PLACEHOLDER*/}
