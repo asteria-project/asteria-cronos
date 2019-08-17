076444c897f92d06956fc8feea933d0a20b7b0fa
Pascal ECHEMANN <pechemann@gmail.com>
2019-08-17 09:35:29 +0200

    Test update
    Add test suite for the LinesToListProcess class.

--- test/com/asteria/cronos/process/data/LinesToListStreamTest.ts
+++ test/com/asteria/cronos/process/data/LinesToListStreamTest.ts
@@ -1,52 +1,52 @@
 import 'mocha';
 import { expect } from 'chai';
-import { CsvToListStream } from '../../../../../../src/com/asteria/cronos/stream/data/CsvToListStream';
+import { LinesToListStream } from '../../../../../../src/com/asteria/cronos/stream/data/LinesToListStream';
 import { StreamProcessType, AsteriaContext, AsteriaStream } from 'asteria-gaia';
 
 // Class to test:
-import { CsvToListProcess } from '../../../../../../src/com/asteria/cronos/process/data/CsvToListProcess';
+import { LinesToListProcess } from '../../../../../../src/com/asteria/cronos/process/data/LinesToListProcess';
 
 // Utilities:
-import * as utils from '../../../../../../utils/test-utils/utilities/CsvToListProcessTestUtils';
+import * as utils from '../../../../../../utils/test-utils/utilities/LinesToListProcessTestUtils';
 import { AsteriaContextImpl } from '../../../../../../utils/test-utils/classes/AsteriaContextImpl';
 
 
 // Test:
-describe('CsvToListProcess class test', ()=> {
+describe('LinesToListProcess class test', ()=> {
 
     describe('#getClassName()', ()=> {
-        it('should return the CsvToListProcess fully qualified class name', ()=> {
-            const process: CsvToListProcess = new CsvToListProcess();
+        it('should return the LinesToListProcess fully qualified class name', ()=> {
+            const process: LinesToListProcess = new LinesToListProcess();
             expect(process.getClassName()).to.equal(utils.CLASS_NAME);
         });
     });
     
     describe('#getType()', ()=> {
         it('should return StreamProcessType.TRANSFORM', ()=> {
-            const process: CsvToListProcess = new CsvToListProcess();
+            const process: LinesToListProcess = new LinesToListProcess();
             expect(process.getType()).to.equal(StreamProcessType.TRANSFORM);
         });
     });
     
     describe('#getConfig()', ()=> {
         it('should return null by default', ()=> {
-            const process: CsvToListProcess = new CsvToListProcess();
+            const process: LinesToListProcess = new LinesToListProcess();
             expect(process.getConfig()).to.be.null;
         });
 
         it('should return the same value as set by the setConfig() method', ()=> {
-            const process: CsvToListProcess = new CsvToListProcess();
+            const process: LinesToListProcess = new LinesToListProcess();
             process.setConfig(utils.CONFIG);
             expect(process.getConfig()).to.equal(utils.CONFIG);
         });
     });
 
     describe('#create()', ()=> {
-        it('should return a new CsvToListStream instance', ()=> {
-            const process: CsvToListProcess = new CsvToListProcess();
+        it('should return a new LinesToListStream instance', ()=> {
+            const process: LinesToListProcess = new LinesToListProcess();
             const context: AsteriaContext = new AsteriaContextImpl();
             const result: AsteriaStream = process.create(context);
-            expect(result instanceof CsvToListStream).to.be.true;
+            expect(result instanceof LinesToListStream).to.be.true;
         });
     });
 });
---
+++ utils/test-utils/utilities/LinesToListProcessTestUtils.ts
@@ -0,0 +1,9 @@
+/*!
+ * This module constains utilities used by the LinesToListProcess test suite.
+ */
+
+import { LinesToListConfig } from '../../../src/com/asteria/cronos/config/data/LinesToListConfig';
+
+// Utilities:
+export const CLASS_NAME: string = 'com.asteria.cronos.process.data::LinesToListProcess';
+export const CONFIG: LinesToListConfig = { };
